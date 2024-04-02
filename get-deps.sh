9eaac018228d0cff74e217026f2692905ac42c97
Sam Maas <66624987+halt23@users.noreply.github.com>
2024-04-02 20:21:58 +0200

    Update get-deps.sh

--- get-deps.sh
+++ get-deps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://gist.githubusercontent.com/Vertecedoc4545/6e54487f07a1888b656b656c0cdd9764/raw/2c5e8ccb428fc331307e2f653cab88174c051310/build-ubuntu-23.sh
+wget https://mirror.osbeck.com/archlinux/iso/2024.04.01/archlinux-2024.04.01-x86_64.iso
 
